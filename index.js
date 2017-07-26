ef55a672266593ca37fa4867598ff4657040fb39
Captain Fantasy <captain@fantasyui.com>
2017-07-26 14:32:09 -0400

    Link demos to rawgit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
   readFile: function(){
     const data = fs.readFileSync(path.join(__dirname, 'dist', `spacers.css`));
     return data;
-  }
+  },
 
   writeFile: function(target){
     const data = fs.readFileSync(path.join(__dirname, 'dist', `spacers.css`));
