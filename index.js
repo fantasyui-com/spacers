a4c67dea0b78c361cefd43f297c18874e78bf8cc
Captain Fantasy <captain@fantasyui.com>
2017-07-26 14:28:06 -0400

    readFile/writeFile helpers

---
+++ index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+module.exports = {
+
+  readFile: function(){
+    const data = fs.readFileSync(path.join(__dirname, 'dist', `spacers.css`));
+    return data;
+  }
+
+  writeFile: function(target){
+    const data = fs.readFileSync(path.join(__dirname, 'dist', `spacers.css`));
+    fs.writeFile(`dist/${name}.html`, data);
+  }
+
+}
